24ea66d451418146b17650a558c15c18019573b8
Andres Valdes <andres@valdes.io>
2018-11-16 12:42:55 -0500

    Banana (#9)
    * adds banana mailer
    
    * adds decision-sending function
    
    * Narrow down error handling

--- src/services/email.js
+++ src/services/email.js
@@ -1,50 +1,51 @@
-import path from 'path';
-import { createTransport } from 'nodemailer';
-import Email from 'email-templates';
+import { Banana } from 'banana-mail';
 
-const { EMAIL, EMAIL_PASSWORD } = process.env;
+const { BANANA_SERVICE, BANANA_EMAIL, BANANA_PASS } = process.env;
 
-const smtpTransport = createTransport({
-  service: 'gmail',
-  auth: { user: EMAIL, pass: EMAIL_PASSWORD },
+const banana = new Banana({
+  service: BANANA_SERVICE,
+  templatePath: 'src/email_templates',
+  auth: {
+    user: BANANA_EMAIL,
+    pass: BANANA_PASS,
+  },
 });
 
-const sendMail = mail => new Promise((resolve, reject) => {
-  smtpTransport.sendMail(mail, (err) => {
-    smtpTransport.close();
-    if (err) reject(err);
-    resolve();
-  });
-});
-
-const loadTemplate = (templateName, hacker) => new Promise(async (resolve, reject) => {
-  const templateDir = path.resolve(`src/templates/emails/${templateName}`);
-  const template = new Email({ views: { options: { extension: 'hbs' } } });
-
+const sendVerification = async (email, token) => {
+  const message = {
+    to: email,
+    subject: '[Peach] Verify Your Email',
+    template: 'verify',
+    context: {
+      token,
+    },
+  };
   try {
-    const hackerTemplate = template.renderAll(templateDir, hacker);
-    resolve(hackerTemplate);
-  } catch (e) {
-    reject(e);
+    await banana.send([message]);
+  } catch (err) {
+    throw err;
   }
-});
+};
 
-const send = (hacker, templateName) => new Promise(async (resolve, reject) => {
-  const hackerTemplate = await loadTemplate(templateName, hacker);
-
-  try {
-    const mail = {
-      to: hacker.email,
-      from: EMAIL,
-      subject: hackerTemplate.subject,
-      html: hackerTemplate.html,
-      text: hackerTemplate.text,
+const sendDecisions = async (hackers, decision) => {
+  const messages = hackers.map((hacker) => {
+    const { email, firstName, lastName } = hacker;
+    const message = {
+      to: email,
+      subject: '[Peach] Update on Your Application',
+      template: decision,
+      context: {
+        firstName,
+        lastName,
+      },
     };
-
-    resolve(sendMail(mail));
-  } catch (e) {
-    reject(e);
+    return message;
+  });
+  try {
+    await banana.send(messages);
+  } catch (err) {
+    throw err;
   }
-});
+};
 
-export default { send };
+export default { sendVerification, sendDecisions };
