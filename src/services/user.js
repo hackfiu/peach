9e7a952544baac98f64037a2cabe2b54e6eefc14
andres-valdes <andres@valdes.io>
2018-11-19 23:04:46 -0500

    fixes the minor bug (for real this time)

--- src/services/user.js
+++ src/services/user.js
@@ -43,7 +43,7 @@ const signUp = async (email, password) => {
     const verificationToken = jwt.sign({ id, verification: true }, SECRET);
     emailService.sendVerification(email, verificationToken);
     const loginToken = jwt.sign({ id, level }, SECRET);
-    return { loginToken };
+    return loginToken;
   } catch (err) {
     throw err;
   }
