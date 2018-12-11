09f93e4c078a0324f4f578a06e63764a3745cad6
carloslahrssen <carloslahrssen@gmail.com>
2018-12-11 17:13:08 -0500

    pushing update

--- src/services/drive.js
+++ src/services/drive.js
@@ -2,9 +2,9 @@ import { google } from 'googleapis';
 
 const { GOOGLE_CLIENT_EMAIL, GOOGLE_PRIVATE_KEY, GOOGLE_FOLDER_ID } = process.env;
 
-const client = new google.auth.JWT(GOOGLE_CLIENT_EMAIL, null, GOOGLE_PRIVATE_KEY, ['https://www.googleapis.com/auth/drive']);
+const auth = new google.auth.JWT(GOOGLE_CLIENT_EMAIL, null, GOOGLE_PRIVATE_KEY, ['https://www.googleapis.com/auth/drive']);
 
-client.authorize()
+auth.authorize()
   .then(() => console.log(`> Connected to Google Drive on ${GOOGLE_CLIENT_EMAIL}`))
   .catch(err => console.error(err));
 
@@ -23,7 +23,16 @@ const upload = async (file) => {
   const media = { mimeType, body };
   const parents = [GOOGLE_FOLDER_ID];
   try {
-    const { webViewLink } = await drive.files.create({ resource, media, parents });
+    console.log('Hit');
+    const { webViewLink } = await drive.files.create({
+      auth,
+      resource,
+      media,
+      parents,
+    });
+
+    console.log(webViewLink);
+    console.log('Success');
     return webViewLink;
   } catch (err) {
     throw err;
